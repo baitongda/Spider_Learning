41e367416a7ff3b09fc0bf23565345570c7d8d4a
linxs <cakelinxs@outlook.com>
2018-05-24 09:29:58 +0800

    add scheduler module

--- Spider_Chapter07/check.py
+++ Spider_Chapter07/check.py
@@ -10,11 +10,12 @@ from proxy_store import RedisClient
 import aiohttp
 import asyncio
 from time import sleep
+from aiohttp import ClientError,ClientConnectorError
+import requests
 
 class CheckUp(object):
     def __init__(self):
         self.db = RedisClient()
-        pass
     
 
 
@@ -25,12 +26,13 @@ class CheckUp(object):
         # pack a task list
         # call run to exec asynic
 
+        print("Start test")
         try:
             proxies = self.db.all()
             loop = asyncio.get_event_loop()
             for i in range(0,len(proxies),BATCH_SIZE):
                 tasks_proxies = proxies[i:i + BATCH_SIZE]
-                tasks = [self.test_single_proxy(proxy) for proxy in tasks_proxies]
+                tasks = [self.check_single_proxy(proxy) for proxy in tasks_proxies]
                 loop.run_until_complete(asyncio.wait(tasks))
                 sleep(3)
         except Exception as e:
@@ -39,14 +41,17 @@ class CheckUp(object):
             print("CheckUp occurs Error")
 
     async def check_single_proxy(self,proxy):
-        connection = aiohttp.TCPConnector(verify=False)
-        async with aiohttp.ClientSession(conn=connection) as session:
+        connection = aiohttp.TCPConnector(verify_ssl=False)
+        async with aiohttp.ClientSession(connector=connection) as session:
             try:
                 if isinstance(proxy,bytes):
                     proxy = proxy.decode('utf-8')
-                real_proxy = "http://{}".format(proxy) 
-                async with session.get(TEST_URL,proxy=real_proxy,timeout=10) as response:
-                    if response.status_code in VAILD_STATUS_CODE:
+                real_proxy = 'http://' + proxy 
+                print(real_proxy)
+                async with session.get(TEST_URL,proxy=real_proxy,timeout=15) as response:
+                    print("Status Code {} ".format(response.status))
+                    print("*****************************")
+                    if response.status in VAILD_STATUS_CODE:
                         self.db.max(proxy)
                         print("Proxy can use")
                     else:
