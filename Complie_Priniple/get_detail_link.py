9627452ccf848a09a9f7e7b5b74db08cf340c2ed
linxs953 <cakelinxs@outlook.com>
2018-07-21 22:21:27 +0800

    init commits

---
+++ Complie_Priniple/get_detail_link.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+
+class GetDetailLink:
+    def __init__(self):
+        pass
+    
+
+    def open(self):
+        pass
+    
+
+    def close(self):
+        pass
+    
+
+    def enter(self):
+        pass
+    
+    def click_chapter_tag(self):
+        pass
+    
+    
